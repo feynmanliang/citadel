bf6b1875f5e0e3c8d470cdfebee7fcce9cfb640e
Feynman Liang <feynman.liang@gmail.com>
2016-03-30 21:42:55 -0700

    Adds YCM build

--- build.zsh
+++ build.zsh
@@ -11,6 +11,11 @@ homeshick link
 ./myrepos/mr checkout
 homeshick link
 
+# install vim plugins
 vim -c "VundleInstall"
+
+# compile YouCompleteMe vim plugin (requires build-essential, cmake, python-dev, python3-dev)
+cd ~/.vim/bundle/YouCompleteMe/
+./install.py --clang-completer --tern-completer # requires nodejs, npm
 
 print "Built citadel and castles undr ~/.homesick/repos. It is now OK to remove this directory"
