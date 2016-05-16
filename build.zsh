b6eba34daf09d37722bdae40247240e77b9d756f
Feynman Liang <feynman.liang@gmail.com>
2016-05-16 13:59:57 +0100

    Fix install script

--- build.zsh
+++ build.zsh
@@ -14,12 +14,19 @@ source $HOME/.homesick/repos/homeshick/homeshick.sh
 homeshick link
 
 # install vim plugins
-print "Installing Vundle plugins"
+print "Installing Vundle and plugins"
+cd $HOME ~/.homesick/repos/vim-castle/
+git submodule init
+git submodule update --recursive
 vim -c "VundleInstall"
 
 # compile YouCompleteMe vim plugin (requires build-essential, cmake, python-dev, python3-dev)
 print "Compiling YouCompleteMe"
-cd ~/.vim/bundle/YouCompleteMe/
+cd $HOME/.vim/bundle/YouCompleteMe/
 ./install.py --clang-completer --tern-completer # requires nodejs, npm
 
+# set private key permissions
+chmod 0600 $HOME/.ssh/id_rsa
+
 print "The kingdom was successfully built."
+print "Citadel now lives in ~/.homeshick/repos/citadel. This directory may now be deleted"
