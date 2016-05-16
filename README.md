b576edf521ebb4d4a91982788d7a7f5c9e68b9be
Feynman Liang <feynman.liang@gmail.com>
2016-05-16 13:44:20 +0100

    Fixes build scripts

---
+++ README.md
@@ -0,0 +1,6 @@
+# Citadel
+Sets up all the `homeshick` castles. Depends on:
+ * `git`
+ * `bash`
+
+Optional: Add `secret-castle` to `mr` if needed.
--- build.zsh
+++ build.zsh
@@ -6,8 +6,8 @@ git clone https://github.com/joeyh/myrepos $HOME/.homesick/repos/myrepos
 git clone https://github.com/feynmanliang/citadel  $HOME/.homesick/repos/citadel
 
 print "Checking out kingdom to ~/.homesick/repos/"
+cd $HOME
 $HOME/.homesick/repos/myrepos/mr checkout
-
 
 print "Symlinking dotfiles to ${HOME}"
 source $HOME/.homesick/repos/homeshick/homeshick.sh
@@ -16,7 +16,6 @@ homeshick link
 # install vim plugins
 print "Installing Vundle plugins"
 vim -c "VundleInstall"
-
 
 # compile YouCompleteMe vim plugin (requires build-essential, cmake, python-dev, python3-dev)
 print "Compiling YouCompleteMe"
