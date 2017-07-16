e5b2d412e683508a813a0811b4576f35f3e4e773
Feynman Liang <feynman.liang@gmail.com>
2017-07-16 13:59:06 -0700

    Update plug install

--- build.zsh
+++ build.zsh
@@ -33,15 +33,15 @@ pyenv which python  # Note the path
 pyenv activate neovim3
 pip install neovim
 pyenv which python  # Note the path
-pip install flake8
-ln -s `pyenv which flake8` ~/bin/flake8  # Assumes that $HOME/bin is in $PATH
+# pip install flake8
+# ln -s `pyenv which flake8` ~/bin/flake8  # Assumes that $HOME/bin is in $PATH
 
 # install vim plugins
 print "Installing plug.vim"
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 print "Installing vim plugins"
-nvim -c "PlugInstall"
+nvim +PlugInstall +qall
 
 # set private key permissions
 chmod 0600 $HOME/.ssh/id_rsa
