cc6d21fcbbf897b96022f45e5f94d50c368fa638
Feynman Liang <feynmanliang@users.noreply.github.com>
2019-02-25 16:07:17 -0800

    Create install_ubuntu.sh

---
+++ install_ubuntu.sh
@@ -0,0 +1,17 @@
+sudo apt-get -y install \
+        software-properties-common \
+        dirmngr
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get -y install \
+        neovim \
+        git \
+        zsh \
+        curl \
+        libreadline-dev \
+        zlib1g-dev \
+        libbz2-dev \
+        libsqlite3-dev \
+        libssl-dev \
+        libffi-dev \
+        gawk
